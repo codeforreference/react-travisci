2ed36db70c51a173550e6f118be4487b223f4a69
codeforreference <37190385+codeforreference@users.noreply.github.com>
2019-05-17 22:59:04 +0530

    Added text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. by Jije
+          peppercloud assignment submitted by Jijendiran
         </p>
         <a
           className="App-link"
