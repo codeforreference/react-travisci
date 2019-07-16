e1151e1a6f64a5b98f1649b5a3e5c90a53d92785
codeforreference <37190385+codeforreference@users.noreply.github.com>
2019-07-16 16:33:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hai This is Jijendiran
+           CodeForReference
         </p>
         <a
           className="App-link"
